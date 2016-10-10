7425ea9c27afde5acb45694f2678cee5266cd6d8
J. Tran <j.yn.tran@gmail.com>
2016-10-10 22:53:50 +0000

    Added response support

--- json-api.py
+++ json-api.py
@@ -1,9 +1,22 @@
-from flask import Flask, request, json
+from flask import Flask, request, Response, json
 app = Flask(__name__)
 
 @app.route('/')
 def api_root():
     return 'Welcome'
+
+@app.route('/hello', methods = [ 'GET' ])
+def api_hello():
+    data = {
+        'hello'  : 'world',
+        'number' : 3
+    }
+    js = json.dumps(data)
+
+    resp = Response(js, status=200, mimetype='application/json')
+    resp.headers['Link'] = 'http://jyntran.ca'
+
+    return resp
 
 @app.route('/messages', methods = ['POST'])
 def api_message():
